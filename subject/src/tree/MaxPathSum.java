8d3d6cbad7967666c4b795b167fccd8235943d02
shengxiang.xiong <823416203@qq.com>
2019-10-28 11:08:29 +0800

    dfs递归子树
    Signed-off-by: shengxiang.xiong <823416203@qq.com>

---
+++ subject/src/tree/MaxPathSum.java
@@ -0,0 +1,64 @@
+package tree;
+
+/**
+ * Created By ShengXiang.Xiong on 2019/10/26
+ */
+public class MaxPathSum {
+    static int ans;
+
+    /**
+     * DFS递归遍历所有子树。
+     * 我们从最高层树着手，其最大值来源于(必须要包括经过其本身根节点，否则线路就断了)：
+     * 1.Max(左子树的整体最大路径和+root.val,右子树的整体最大路径和+root.val)
+     * 2.左子树边路最大路径和+右子树边路最大路径和+root.val
+     * 3.root.val
+     * 解释一下：为什么要有边路（单回溯路径），因为只有边路才可以连接左右子树
+     * 所以另外我们还得分析每一个子树的边路递归形式，其最大边路和来自于
+     * 1. Max(左子树边路和+root.val,右子树边路和+root.val）
+     * 2. root.val
+     * <p>
+     * 那我们设计递归函数就可以知道，需要保存两个值，一是当前子树的最大路径和sum[0]，二是当前子树的最大边路和sum[1]
+     * 另外在遍历所有树时，需要时时更新ans=Max(ans,sum[0])
+     *
+     * @param root
+     * @return
+     */
+    public static int[] dfs(TreeNode root) {
+        //sum[0]表示当前子树的最大和，sum[1]表示当前子树的边路最大和（上层树可能会用到）
+        int[] sum = new int[2];
+        if (root.right == null && root.left == null) {
+            sum[0] = root.val;
+            sum[1] = root.val;
+        } else {
+            int[] l = new int[2];
+            int[] r = new int[2];
+            l[0] = -1000000000;
+            l[1] = l[0];
+            r[0] = l[0];
+            r[1] = l[0];
+            if (root.right != null) {
+                r = dfs(root.right);
+            }
+            if (root.left != null) {
+                l = dfs(root.left);
+            }
+            sum[0] = Math.max(root.val, Math.max(Math.max(l[0] + root.val, r[0] + root.val), l[1] + r[1] + root.val));
+            sum[1] = Math.max(root.val, Math.max(l[1] + root.val, r[1] + root.val));
+        }
+        ans = Math.max(ans, sum[0]);
+        return sum;
+    }
+
+
+    public static int maxPathSum(TreeNode root) {
+        String s = "-10,9,20,null,null,15,-7";
+        TreeNode.bstFromPreorder(root, s.split("[, ]"));
+        ans = root.val;
+        dfs(root);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxPathSum(new TreeNode(0)));
+    }
+}
---
+++ subject/src/tree/TreeNode.java
@@ -0,0 +1,85 @@
+package tree;
+
+import java.util.*;
+
+/**
+ * Created By ShengXiang.Xiong on 2019/10/26
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    public static ArrayList<Integer> preOrder(TreeNode root) {
+        ArrayList path = new ArrayList();
+        if (root.left != null) {
+            path.addAll(preOrder(root.left));
+        }
+        path.add(root.val);
+        if (root.right != null) {
+            path.addAll(preOrder(root.right));
+        }
+        return path;
+    }
+
+    public static ArrayList<Integer> inOrder(TreeNode root) {
+        ArrayList path = new ArrayList();
+        path.add(root.val);
+        if (root.left != null) {
+            path.addAll(inOrder(root.left));
+        }
+        if (root.right != null) {
+            path.addAll(inOrder(root.right));
+        }
+        return path;
+    }
+
+    public static ArrayList<Integer> postOrder(TreeNode root) {
+        ArrayList path = new ArrayList();
+        if (root.right != null) {
+            path.addAll(postOrder(root.right));
+        }
+        if (root.left != null) {
+            path.addAll(postOrder(root.left));
+        }
+        path.add(root.val);
+        return path;
+    }
+
+    public static void bstFromPreorder(TreeNode root, String[] nums) {
+        LinkedList<String> lk = new LinkedList<>(Arrays.asList(nums));
+        if (lk.size() > 0) {
+            Queue<TreeNode> q = new ArrayDeque<>();
+            if (!lk.peek().matches("null|#")) {
+                root.val = Integer.valueOf(lk.poll());
+                q.add(root);
+                while (q.size() > 0 && lk.size() > 0) {
+                    int s = q.size();
+                    while (s-- > 0 && lk.size() > 0) {
+                        TreeNode cr = q.poll();
+                        String l = lk.poll();
+                        String r = "#";
+                        if (lk.size() > 0) {
+                            r = lk.poll();
+                        }
+                        assert l != null;
+                        if (!l.matches("null|#")) {
+                            TreeNode lchild = new TreeNode(Integer.valueOf(l));
+                            cr.left = lchild;
+                            q.add(lchild);
+                        }
+                        if (!r.matches("null|#")) {
+                            TreeNode rchild = new TreeNode(Integer.valueOf(r));
+                            cr.right = rchild;
+                            q.add(rchild);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
